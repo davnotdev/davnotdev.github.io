7872c8c05a7e26e687ac47982996f2feae2d2927
dav <usr21p1@gmail.com>
2023-02-25 23:42:01 -0800

    Change davnotdev.github.io/www to just davnotdev.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://davnotdev.github.io',
-    base: '/www',
+    base: '/',
 });
